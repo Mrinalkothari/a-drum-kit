9ce0994cf48cf663d1b0c7908a1255f3976a50d5
Mrinal Kothari <48431413+Mrinalkothari@users.noreply.github.com>
2020-02-21 18:26:56 +0530

    js added

---
+++ drum.js
@@ -0,0 +1,69 @@
+// finding number of buttons
+
+var numberOfDrumButtons = document.querySelectorAll(".drum").length;
+// looping to go through all the buttons
+for (var i = 0; i<numberOfDrumButtons; i++) {
+    // working with click
+    document.querySelectorAll(".drum")[i].addEventListener("click", function() {
+       
+           var x = this.innerHTML;
+            
+       makeSound(x);
+       buttonAnimation(x);
+       
+    });
+}
+
+// working with the key
+document.addEventListener("keypress", function(event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+});
+
+// function that will make sound
+function makeSound(key) {
+
+    switch (key) {
+        case "w": 
+        var tom1 = new Audio("sounds/tom-1.mp3");
+        tom1.play();
+        break;
+        case "a": 
+        var tom2 = new Audio("sounds/tom-2.mp3");
+        tom2.play();
+        break;
+        case "s": 
+        var tom3 = new Audio("sounds/tom-3.mp3");
+        tom3.play();
+        break;
+        case "d": 
+        var tom4 = new Audio("sounds/tom-4.mp3");
+        tom4.play();
+        break;
+        case "j": 
+        var snare = new Audio("sounds/snare.mp3");
+        snare.play();
+        break;
+        case "k": 
+        var crash = new Audio("sounds/crash.mp3");
+        crash.play();
+        break;
+        case "l": 
+        var kick = new Audio("sounds/kick-bass.mp3");
+        kick.play();
+            
+            break;
+     
+            
+    }
+
+}
+// for the button Animation
+function buttonAnimation(currentKey) {
+    var activeKey = document.querySelector("." + currentKey);
+    activeKey.classList.add("pressed");
+
+    setTimeout(()=> {
+        activeKey.classList.remove("pressed"), 100
+    });
+}
